79a1c60c6ca0801540fc3aecb399871402a51f1f
Yanir Aton <87027341+yaniraton@users.noreply.github.com>
2024-02-28 09:09:49 +0200

    Update myscripts.js

--- public/myscripts.js
+++ public/myscripts.js
@@ -61,7 +61,7 @@ document.querySelector('form').addEventListener('submit', function(event) {
   // Send the request asynchronously
   // the body should be an json with key value pairs
   console.log(event.target.action);
-  fetch("https://courageous-cyan-robe.cyclic.app/", {
+  fetch("/", {
     method: 'POST',
     body: JSON.stringify(formFields),
     headers: {
@@ -90,9 +90,14 @@ document.querySelector('form').addEventListener('submit', function(event) {
   // Generate QR code if there is an error print it to the console
   qrText = valuesString;
 
+  if (qrText.length < 221 && qrText.length > 180)  {
+    // add " " to the qr code at least 200 characters
+    qrText = qrText + " ".repeat(200 - qrText.length);
+    console.log(qrText);
+  }
+
   // split the qr into two qr codes
-  QRCode1.makeCode(qrText.substring(0, qrText.length / 2));
-  QRCode2.makeCode(qrText.substring(qrText.length / 2, qrText.length));
+  QRCode1.makeCode(qrText.substring(0, qrText.length));
 
   // set all the forms filds to empty exsect the readonly ones, set them to 0
   var inputs = document.getElementById("Scouting_Form").getElementsByTagName("input");
